5509da1fb041715c0aead548459228f26d46b7bc
REL5656 <blsmk.260127060@budiluhur.sch.id>
2026-08-21 11:38:27 +0700

    Create rfk.js

---
+++ rfk.js
@@ -0,0 +1,19 @@
+let nilai = 85;
+
+switch(true){
+
+    case (nilai >= 90):
+        console.log("Nilai A");
+        break;
+
+    case (nilai >= 80):
+        console.log("Nilai B");
+        break;
+
+    case (nilai >= 70):
+        console.log("Nilai C");
+        break;
+
+    default:
+        console.log("Nilai D");
+}
